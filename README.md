a513cdf99bbfcdb765c2d08fc97b033548045d17
Plumbheater <plumbheater@yandex.ru>
2022-12-08 10:56:06 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+[![Build status](https://ci.appveyor.com/api/projects/status/roi45cnirf3xefie/branch/main?svg=true)](https://ci.appveyor.com/project/Plumbheater/autojavahw-l1-2-2/branch/main)
---
+++ src/test/java/TestV1.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+public class TestV1 {
+@Test
+    void test1 () {
+    // Given - When - Then
+// Предусловия
+    given()
+            .baseUri("https://postman-echo.com")
+            .body("some data")
+            .when()
+            .post("/post")
+            .then()
+            .statusCode(200)
+            .body("data", equalTo("some data"));
+    }
+}
